375587a1193d0e7f5d6863b5b6eca7ec193c8ec9
Elorm Buertey <ebuertey5@gmail.com>
2022-11-05 23:25:35 +0000

    Add base server code

--- src/alewa.cpp
+++ src/alewa.cpp
@@ -1,19 +1,16 @@
-#include "net/network.hpp"
 #include "net/sysnet_provider.hpp"
+
+#include "server.hpp"
 
 int main(/*int argc, char* argv[]*/)
 {
+    using namespace alewa;
     using namespace alewa::net;
 
-    SystemNetworkProvider::AddrInfo hints{};
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
     SystemNetworkProvider api;
-    AddrInfoList<SystemNetworkProvider> spec{api, nullptr, "3490", &hints};
-    Socket<SystemNetworkProvider> socket{api, spec};
-    socket.bind(*spec.cur());
+    Server server{api};
+
+    server.start("8080", 10);
 
     return 0;
 }
---
+++ src/alewa/server.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <string>
+
+#include "net/network.hpp"
+
+namespace alewa {
+
+namespace detail {
+
+template <net::NetworkProvider T>
+auto hints() -> typename T::AddrInfo;
+
+}  // namespace alewa::detail
+
+template <net::NetworkProvider T>
+class Server
+{
+private:
+    T const & api;
+
+public:
+    Server(T const & api) : api(api) {}
+
+    void start(std::string const & port, int backlog);
+    void stop() {}
+};
+
+template <net::NetworkProvider T>
+void Server<T>::start(std::string const & port, int backlog)
+{
+    typename T::AddrInfo hints = detail::hints<T>();
+    net::AddrInfoList<T> spec{api, nullptr, port.c_str(), &hints};
+    net::Socket<T> socket{api, spec};
+
+    socket.bind(*spec.cur());
+    socket.listen(backlog);
+
+    net::SockInfo<T> info;
+    net::Socket<T> accepted = socket.accept(info);
+}
+
+namespace detail {
+#include <netdb.h>
+
+template <net::NetworkProvider T>
+auto hints() -> typename T::AddrInfo
+{
+    typename T::AddrInfo hints{};
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_socktype = SOCK_STREAM;
+    return hints;
+}
+}  // namespace alewa::detail
+
+}  // namespace alewa
